2b6a135687fdb07f44da8e629ca7b6cb1942b60c
Marada431 <aminatrox@wp.pl>
2026-06-01 11:33:20 +0200

    Conflict reosolution

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.php');
+    exit;
+}
 
 // 1. PRZEKIEROWANIE JEŚLI UŻYTKOWNIK NIE JEST ZALOGOWANY
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
